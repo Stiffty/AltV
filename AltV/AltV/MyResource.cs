61f189e14a897d58556139950c399f6ac814a1f1
elach <el.ach@gmx.de>
2020-04-14 15:56:36 +0200

    AltV init

---
+++ AltV/AltV/MyResource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AltV.Net;
+
+namespace AltV
+{
+    internal class MyResource : Resource
+    {
+        public override void OnStart()
+        {
+            Console.WriteLine("Started");
+        }
+
+        public override void OnStop()
+        {
+            Console.WriteLine("Stopped");
+        }
+    }
+}
