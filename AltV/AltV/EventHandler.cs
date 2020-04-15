b4d24427ab070f206efdfc2bd90d2bad1cb2cb55
elach <el.ach@gmx.de>
2020-04-15 18:48:11 +0200

    fix no movment in menu

---
+++ AltV/AltV/EventHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AltV.Net;
+using AltV.Net.Data;
+using AltV.Net.Elements.Entities;
+using AltV.Net.Enums;
+
+namespace AltV
+{
+    class EventHandler : IScript
+    {
+        [ScriptEvent(ScriptEventType.PlayerConnect)]
+        public static void PlayerConnect(IPlayer player,string reason)
+        {
+            player.SetPosition(-1070.06250f,-2975.122803f,13.773568f);
+            player.SetDateTime(DateTime.Now);
+            player.Model = ( uint ) PedModel.FreemodeMale01;
+            Console.WriteLine($"New Player joined: {player.Name}");
+        }
+
+        public static void MyMessageHandler(IPlayer player,string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void SpawnVehicle(IPlayer player)
+        {
+            string VehicleName = "scramjet";
+            IVehicle veh = Alt.CreateVehicle(Alt.Hash(VehicleName), new Position(player.Position.X, player.Position.Y + 1.5f, player.Position.Z), player.Rotation);
+        }
+    }
+}
--- AltV/AltV/MyResource.cs
+++ AltV/AltV/MyResource.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AltV.Net;
+using AltV.Net.Elements.Entities;
 
 namespace AltV
 {
@@ -7,8 +8,15 @@ namespace AltV
     {
         public override void OnStart()
         {
+            Alt.On<IPlayer,string>("myMessage",EventHandler.MyMessageHandler);
+            Alt.On<IPlayer>("spawnVehicle",EventHandler.SpawnVehicle);
+            //debug
+            Alt.OnServerEvent += (name,args) => { Alt.Log(name + " " + args.Length); };
+            Alt.OnPlayerEvent += (player,name,args) => { Alt.Log("event:" + name); };
             Console.WriteLine("Started");
         }
+
+       
 
         public override void OnStop()
         {
